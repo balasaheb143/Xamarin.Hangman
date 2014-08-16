2d291909c5993c3fe7916455bba6dd2d7421e6ed
Demian Flavius <flaviusdemian91@yahoo.com>
2014-08-16 16:51:28 +0300

    added circle cropper

--- Fundora.Hangman/Fundora.Hangman/Resources/Resource.Designer.cs
+++ Fundora.Hangman/Fundora.Hangman/Resources/Resource.Designer.cs
@@ -26,10 +26,84 @@ namespace Fundora.Hangman
 		
 		public static void UpdateIdValues()
 		{
+			global::CircleImageCropper.Resource.Array.showGuidelinesArray = global::Fundora.Hangman.Resource.Array.showGuidelinesArray;
+			global::CircleImageCropper.Resource.Attribute.aspectRatioX = global::Fundora.Hangman.Resource.Attribute.aspectRatioX;
+			global::CircleImageCropper.Resource.Attribute.aspectRatioY = global::Fundora.Hangman.Resource.Attribute.aspectRatioY;
+			global::CircleImageCropper.Resource.Attribute.fixAspectRatio = global::Fundora.Hangman.Resource.Attribute.fixAspectRatio;
+			global::CircleImageCropper.Resource.Attribute.guidelines = global::Fundora.Hangman.Resource.Attribute.guidelines;
+			global::CircleImageCropper.Resource.Attribute.imageResource = global::Fundora.Hangman.Resource.Attribute.imageResource;
+			global::CircleImageCropper.Resource.Color.black_translucent = global::Fundora.Hangman.Resource.Color.black_translucent;
+			global::CircleImageCropper.Resource.Dimension.activity_horizontal_margin = global::Fundora.Hangman.Resource.Dimension.activity_horizontal_margin;
+			global::CircleImageCropper.Resource.Dimension.activity_vertical_margin = global::Fundora.Hangman.Resource.Dimension.activity_vertical_margin;
+			global::CircleImageCropper.Resource.Dimension.textSize = global::Fundora.Hangman.Resource.Dimension.textSize;
+			global::CircleImageCropper.Resource.Drawable.bkg = global::Fundora.Hangman.Resource.Drawable.bkg;
+			global::CircleImageCropper.Resource.Drawable.butterfly = global::Fundora.Hangman.Resource.Drawable.butterfly;
+			global::CircleImageCropper.Resource.Drawable.ic_launcher = global::Fundora.Hangman.Resource.Drawable.ic_launcher;
+			global::CircleImageCropper.Resource.Drawable.Icon = global::Fundora.Hangman.Resource.Drawable.Icon;
+			global::CircleImageCropper.Resource.Drawable.sidewayssky = global::Fundora.Hangman.Resource.Drawable.sidewayssky;
+			global::CircleImageCropper.Resource.Drawable.violetsky = global::Fundora.Hangman.Resource.Drawable.violetsky;
+			global::CircleImageCropper.Resource.Id.CropOverlayView = global::Fundora.Hangman.Resource.Id.CropOverlayView;
+			global::CircleImageCropper.Resource.Id.ImageView_image = global::Fundora.Hangman.Resource.Id.ImageView_image;
+			global::CircleImageCropper.Resource.Id.off = global::Fundora.Hangman.Resource.Id.off;
+			global::CircleImageCropper.Resource.Id.on = global::Fundora.Hangman.Resource.Id.on;
+			global::CircleImageCropper.Resource.Id.onTouch = global::Fundora.Hangman.Resource.Id.onTouch;
+			global::CircleImageCropper.Resource.Layout.crop_image_view = global::Fundora.Hangman.Resource.Layout.crop_image_view;
+			global::CircleImageCropper.Resource.String.action_settings = global::Fundora.Hangman.Resource.String.action_settings;
+			global::CircleImageCropper.Resource.String.app_name = global::Fundora.Hangman.Resource.String.app_name;
+			global::CircleImageCropper.Resource.String.aspectRatioX = global::Fundora.Hangman.Resource.String.aspectRatioX;
+			global::CircleImageCropper.Resource.String.aspectRatioXHeader = global::Fundora.Hangman.Resource.String.aspectRatioXHeader;
+			global::CircleImageCropper.Resource.String.aspectRatioY = global::Fundora.Hangman.Resource.String.aspectRatioY;
+			global::CircleImageCropper.Resource.String.aspectRatioYHeader = global::Fundora.Hangman.Resource.String.aspectRatioYHeader;
+			global::CircleImageCropper.Resource.String.crop = global::Fundora.Hangman.Resource.String.crop;
+			global::CircleImageCropper.Resource.String.croppedImageDesc = global::Fundora.Hangman.Resource.String.croppedImageDesc;
+			global::CircleImageCropper.Resource.String.fixedAspectRatio = global::Fundora.Hangman.Resource.String.fixedAspectRatio;
+			global::CircleImageCropper.Resource.String.rotate = global::Fundora.Hangman.Resource.String.rotate;
+			global::CircleImageCropper.Resource.String.showGuidelines = global::Fundora.Hangman.Resource.String.showGuidelines;
+			global::CircleImageCropper.Resource.String.title = global::Fundora.Hangman.Resource.String.title;
+			global::CircleImageCropper.Resource.Style.AppBaseTheme = global::Fundora.Hangman.Resource.Style.AppBaseTheme;
+			global::CircleImageCropper.Resource.Style.AppTheme = global::Fundora.Hangman.Resource.Style.AppTheme;
+			global::CircleImageCropper.Resource.Style.RoboTheme = global::Fundora.Hangman.Resource.Style.RoboTheme;
+			global::CircleImageCropper.Resource.Styleable.CropImageView = global::Fundora.Hangman.Resource.Styleable.CropImageView;
+			global::CircleImageCropper.Resource.Styleable.CropImageView_aspectRatioX = global::Fundora.Hangman.Resource.Styleable.CropImageView_aspectRatioX;
+			global::CircleImageCropper.Resource.Styleable.CropImageView_aspectRatioY = global::Fundora.Hangman.Resource.Styleable.CropImageView_aspectRatioY;
+			global::CircleImageCropper.Resource.Styleable.CropImageView_fixAspectRatio = global::Fundora.Hangman.Resource.Styleable.CropImageView_fixAspectRatio;
+			global::CircleImageCropper.Resource.Styleable.CropImageView_guidelines = global::Fundora.Hangman.Resource.Styleable.CropImageView_guidelines;
+			global::CircleImageCropper.Resource.Styleable.CropImageView_imageResource = global::Fundora.Hangman.Resource.Styleable.CropImageView_imageResource;
+		}
+		
+		public partial class Array
+		{
+			
+			// aapt resource value: 0x7f080000
+			public const int showGuidelinesArray = 2131230720;
+			
+			static Array()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Array()
+			{
+			}
 		}
 		
 		public partial class Attribute
 		{
+			
+			// aapt resource value: 0x7f010002
+			public const int aspectRatioX = 2130771970;
+			
+			// aapt resource value: 0x7f010003
+			public const int aspectRatioY = 2130771971;
+			
+			// aapt resource value: 0x7f010001
+			public const int fixAspectRatio = 2130771969;
+			
+			// aapt resource value: 0x7f010000
+			public const int guidelines = 2130771968;
+			
+			// aapt resource value: 0x7f010004
+			public const int imageResource = 2130771972;
 			
 			static Attribute()
 			{
@@ -41,14 +115,67 @@ namespace Fundora.Hangman
 			}
 		}
 		
+		public partial class Color
+		{
+			
+			// aapt resource value: 0x7f050000
+			public const int black_translucent = 2131034112;
+			
+			static Color()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Color()
+			{
+			}
+		}
+		
+		public partial class Dimension
+		{
+			
+			// aapt resource value: 0x7f060000
+			public const int activity_horizontal_margin = 2131099648;
+			
+			// aapt resource value: 0x7f060001
+			public const int activity_vertical_margin = 2131099649;
+			
+			// aapt resource value: 0x7f060002
+			public const int textSize = 2131099650;
+			
+			static Dimension()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Dimension()
+			{
+			}
+		}
+		
 		public partial class Drawable
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			public const int bkg = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int monoandroidsplash = 2130837505;
+			public const int butterfly = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int ic_launcher = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int Icon = 2130837507;
+			
+			// aapt resource value: 0x7f020004
+			public const int monoandroidsplash = 2130837508;
+			
+			// aapt resource value: 0x7f020005
+			public const int sidewayssky = 2130837509;
+			
+			// aapt resource value: 0x7f020006
+			public const int violetsky = 2130837510;
 			
 			static Drawable()
 			{
@@ -63,8 +190,53 @@ namespace Fundora.Hangman
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f060000
-			public const int MyButton = 2131099648;
+			// aapt resource value: 0x7f04000b
+			public const int Button_crop = 2130968587;
+			
+			// aapt resource value: 0x7f040007
+			public const int Button_rotate = 2130968583;
+			
+			// aapt resource value: 0x7f040006
+			public const int CropImageView = 2130968582;
+			
+			// aapt resource value: 0x7f04000e
+			public const int CropOverlayView = 2130968590;
+			
+			// aapt resource value: 0x7f04000d
+			public const int ImageView_image = 2130968589;
+			
+			// aapt resource value: 0x7f04000f
+			public const int action_settings = 2130968591;
+			
+			// aapt resource value: 0x7f04000c
+			public const int croppedImageView = 2130968588;
+			
+			// aapt resource value: 0x7f040004
+			public const int mylayout = 2130968580;
+			
+			// aapt resource value: 0x7f040002
+			public const int off = 2130968578;
+			
+			// aapt resource value: 0x7f040000
+			public const int on = 2130968576;
+			
+			// aapt resource value: 0x7f040001
+			public const int onTouch = 2130968577;
+			
+			// aapt resource value: 0x7f040003
+			public const int scrollview = 2130968579;
+			
+			// aapt resource value: 0x7f040009
+			public const int showGuidelines = 2130968585;
+			
+			// aapt resource value: 0x7f040008
+			public const int showGuidelinesLayout = 2130968584;
+			
+			// aapt resource value: 0x7f04000a
+			public const int showGuidelinesSpin = 2130968586;
+			
+			// aapt resource value: 0x7f040005
+			public const int title = 2130968581;
 			
 			static Id()
 			{
@@ -80,7 +252,10 @@ namespace Fundora.Hangman
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			public const int activity_main = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int crop_image_view = 2130903041;
 			
 			static Layout()
 			{
@@ -92,14 +267,60 @@ namespace Fundora.Hangman
 			}
 		}
 		
+		public partial class Menu
+		{
+			
+			// aapt resource value: 0x7f0a0000
+			public const int main = 2131361792;
+			
+			static Menu()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Menu()
+			{
+			}
+		}
+		
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040001
-			public const int ApplicationName = 2130968577;
+			// aapt resource value: 0x7f070002
+			public const int action_settings = 2131165186;
 			
-			// aapt resource value: 0x7f040000
-			public const int Hello = 2130968576;
+			// aapt resource value: 0x7f070000
+			public const int app_name = 2131165184;
+			
+			// aapt resource value: 0x7f070004
+			public const int aspectRatioX = 2131165188;
+			
+			// aapt resource value: 0x7f070005
+			public const int aspectRatioXHeader = 2131165189;
+			
+			// aapt resource value: 0x7f070006
+			public const int aspectRatioY = 2131165190;
+			
+			// aapt resource value: 0x7f070007
+			public const int aspectRatioYHeader = 2131165191;
+			
+			// aapt resource value: 0x7f070009
+			public const int crop = 2131165193;
+			
+			// aapt resource value: 0x7f07000b
+			public const int croppedImageDesc = 2131165195;
+			
+			// aapt resource value: 0x7f070003
+			public const int fixedAspectRatio = 2131165187;
+			
+			// aapt resource value: 0x7f07000a
+			public const int rotate = 2131165194;
+			
+			// aapt resource value: 0x7f070008
+			public const int showGuidelines = 2131165192;
+			
+			// aapt resource value: 0x7f070001
+			public const int title = 2131165185;
 			
 			static String()
 			{
@@ -114,8 +335,17 @@ namespace Fundora.Hangman
 		public partial class Style
 		{
 			
-			// aapt resource value: 0x7f050000
-			public const int Mono_Android_Theme_Splash = 2131034112;
+			// aapt resource value: 0x7f090000
+			public const int AppBaseTheme = 2131296256;
+			
+			// aapt resource value: 0x7f090001
+			public const int AppTheme = 2131296257;
+			
+			// aapt resource value: 0x7f090003
+			public const int Mono_Android_Theme_Splash = 2131296259;
+			
+			// aapt resource value: 0x7f090002
+			public const int RoboTheme = 2131296258;
 			
 			static Style()
 			{
@@ -123,6 +353,41 @@ namespace Fundora.Hangman
 			}
 			
 			private Style()
+			{
+			}
+		}
+		
+		public partial class Styleable
+		{
+			
+			public static int[] CropImageView = new int[] {
+					2130771968,
+					2130771969,
+					2130771970,
+					2130771971,
+					2130771972};
+			
+			// aapt resource value: 2
+			public const int CropImageView_aspectRatioX = 2;
+			
+			// aapt resource value: 3
+			public const int CropImageView_aspectRatioY = 3;
+			
+			// aapt resource value: 1
+			public const int CropImageView_fixAspectRatio = 1;
+			
+			// aapt resource value: 0
+			public const int CropImageView_guidelines = 0;
+			
+			// aapt resource value: 4
+			public const int CropImageView_imageResource = 4;
+			
+			static Styleable()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Styleable()
 			{
 			}
 		}
